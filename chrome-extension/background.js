36d97d6e608563351ddd8babbaa283395047c515
Dongwon Jung <dongwonj@usc.edu>
2023-01-07 12:05:32 -0800

    Added chrome extention funtionality described in the assignment.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,15 +1,3 @@
-/*chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["./content.js"]
-        })
-            .then(() => {
-                console.log("INJECTED THE FOREGROUND SCRIPT.");
-            })
-            .catch(err => console.log(err));
-    }
-});*/
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -15,7 +15,6 @@ async function getTweets(){
             return response.json()
         })
         .then(data => {
-            console.log(data)
             if (data['is_english'] == true){
                 fetch('http://127.0.0.1:8080/api/sentiment-score',{
                     method: 'POST',
@@ -27,35 +26,44 @@ async function getTweets(){
                     return response.json()
                 })
                 .then(data => {
-                    console.log(data)
-                    data['detected_mood']
+                    detected_mood = data['detected_mood']
+                    let emoji = "<span>&#128528;</span>"
+                    if (detected_mood == 'POSITIVE'){
+                        emoji = "<span>&#128522;</span>"
+                    }
+                    else if (detected_mood == 'Negative'){
+                        emoji = "<span>&#9785;</span>"
+                    }
+
+                    if (tweet.id == ""){
+                        let rootNode = tweet.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
+                        const childNodes = rootNode.childNodes;
+                        if (childNodes.length == 2){
+                            const dot = childNodes[0].cloneNode(true);
+                            const detectedMood = childNodes[0].cloneNode(true);
+                            detectedMood.innerHTML = "Detected Mood: "+emoji;
+                            rootNode.appendChild(dot);
+                            rootNode.appendChild(detectedMood);
+                        }
+                    }
+                    else{
+                        let rootNode = tweet.parentNode.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
+                        const childNodes = rootNode.childNodes;
+                        if (childNodes.length == 3){
+                            const dot = childNodes[1].cloneNode(true);
+                            const detectedMood = childNodes[1].cloneNode(true);
+                            detectedMood.innerHTML = "Detected Mood: "+emoji;
+                            rootNode.appendChild(dot);
+                            rootNode.appendChild(detectedMood);
+                        }
+                        
+                    }
+
                 })
             }
         })
 
-        if (tweet.id == ""){
-            let rootNode = tweet.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
-            const childNodes = rootNode.childNodes;
-            if (childNodes.length == 2){
-                const dot = childNodes[0].cloneNode(true);
-                const detectedMood = childNodes[0].cloneNode(true);
-                detectedMood.innerText = "Detected Mood:";
-                rootNode.appendChild(dot);
-                rootNode.appendChild(detectedMood);
-            }
-        }
-        else{
-            let rootNode = tweet.parentNode.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
-            const childNodes = rootNode.childNodes;
-            if (childNodes.length == 3){
-                const dot = childNodes[1].cloneNode(true);
-                const detectedMood = childNodes[1].cloneNode(true);
-                detectedMood.innerText = "Detected Mood:";
-                rootNode.appendChild(dot);
-                rootNode.appendChild(detectedMood);
-            }
-            
-        }
+        
     }
 }
 
