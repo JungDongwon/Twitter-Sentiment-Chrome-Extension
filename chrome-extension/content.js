d0328541d9c1823b6bbf276acffa37df356dbf5e
Dongwon Jung <dongwonj@usc.edu>
2023-01-08 15:37:02 -0800

    fixed the api input and output format

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,78 +1,90 @@
 // server url 
-BASE_URL = "https://myproject-415.wl.r.appspot.com"
+//BASE_URL = "https://myproject-415.wl.r.appspot.com"
 // if server is running locally, use the url below instead.
-// BASE_URL = "http://127.0.0.1:8080"
+BASE_URL = "http://127.0.0.1:8080"
 
 async function addSentimentOfTweets(){
+    let tweets_dict = {}
     const tweets = document.querySelectorAll(`[data-testid="tweetText"]`);
+    let body = []
     for (let tweet of tweets){
         const tweetText = tweet.innerText 
+        const obj = {[tweetText]: tweet}
+        Object.assign(tweets_dict,obj)
         if (tweetText == ""){
             continue
         }
-        fetch(`${BASE_URL}/api/language-detection`,{
+        else{
+            body.push({tweet_text: tweetText})
+        }
+    }
+    console.log(tweets_dict)
+    fetch(`${BASE_URL}/api/language-detection`,{
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        body = []
+        for (let res of data){
+            if (res['is_english'] == true){
+                body.push({tweet_text: res['tweet_text']})
+            }
+        }
+        fetch(`${BASE_URL}/api/sentiment-score`,{
             method: 'POST',
             mode: 'cors',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({tweet_text: tweetText})
+            body: JSON.stringify(body)
         })
         .then(response => {
             return response.json()
         })
         .then(data => {
-            if (data['is_english'] == true){
-                fetch(`${BASE_URL}/api/sentiment-score`,{
-                    method: 'POST',
-                    mode: 'cors',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({tweet_text: tweetText})
-                })
-                .then(response => {
-                    return response.json()
-                })
-                .then(data => {
-                    detected_mood = data['detected_mood']
-                    let emoji = "<span>&#128528;</span>"
-                    if (detected_mood == 'POSITIVE'){
-                        emoji = "<span>&#128522;</span>"
-                    }
-                    else if (detected_mood == 'Negative'){
-                        emoji = "<span>&#9785;</span>"
-                    }
-
-                    if (tweet.id == ""){
-                        if (tweet.parentNode.parentNode.parentNode.getElementsByTagName("time").length != 0){
-                            let rootNode = tweet.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
-                            const childNodes = rootNode.childNodes;
-                            if (childNodes.length == 2){
-                                const dot = childNodes[0].cloneNode(true);
-                                const detectedMood = childNodes[0].cloneNode(true);
-                                detectedMood.innerHTML = "Detected Mood: "+emoji;
-                                rootNode.appendChild(dot);
-                                rootNode.appendChild(detectedMood);
-                            }
+            for (let res of data){
+                detected_mood = res['detected_mood']
+                let emoji = "<span>&#128528;</span>"
+                if (detected_mood == 'POSITIVE'){
+                    emoji = "<span>&#128522;</span>"
+                }
+                else if (detected_mood == 'Negative'){
+                    emoji = "<span>&#9785;</span>"
+                }
+                const tweet  = tweets_dict[res['tweet_text']]
+                if (tweet.id = ""){
+                    if (tweet.parentNode.parentNode.parentNode.getElementsByTagName("time").length != 0){
+                        let rootNode = tweet.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
+                        const childNodes = rootNode.childNodes;
+                        if (childNodes.length == 2){
+                            const dot = childNodes[0].cloneNode(true);
+                            const detectedMood = childNodes[0].cloneNode(true);
+                            detectedMood.innerHTML = "Detected Mood: "+emoji;
+                            rootNode.appendChild(dot);
+                            rootNode.appendChild(detectedMood);
                         }
                     }
-                    else{
-                        if (tweet.parentNode.parentNode.parentNode.getElementsByTagName("time").length != 0){
-                            let rootNode = tweet.parentNode.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
-                            const childNodes = rootNode.childNodes;
-                            if (childNodes.length == 3){
-                                const dot = childNodes[1].cloneNode(true);
-                                const detectedMood = childNodes[1].cloneNode(true);
-                                detectedMood.innerHTML = "Detected Mood: "+emoji;
-                                rootNode.appendChild(dot);
-                                rootNode.appendChild(detectedMood);
-                            }
+                }
+                else{
+                    if (tweet.parentNode.parentNode.parentNode.getElementsByTagName("time").length != 0){
+                        let rootNode = tweet.parentNode.parentNode.parentNode.getElementsByTagName("time")[0].parentNode.parentNode.parentNode;
+                        const childNodes = rootNode.childNodes;
+                        if (childNodes.length == 3){
+                            const dot = childNodes[1].cloneNode(true);
+                            const detectedMood = childNodes[1].cloneNode(true);
+                            detectedMood.innerHTML = "Detected Mood: "+emoji;
+                            rootNode.appendChild(dot);
+                            rootNode.appendChild(detectedMood);
                         }
                     }
-
-                })
+                }
             }
-        })
-
-        
-    }
+        }) 
+    })
 }
+
 
 addSentimentOfTweets();
--- main.py
+++ main.py
@@ -14,49 +14,52 @@ SENTIMENT_SCORE = SentimentScore()
 @app.route('/api/language-detection',methods=['POST'])
 def language_detection():
     data = request.json
-    tweet_text = data['tweet_text']
-    tweet_text = tweet_text.replace('\n', ' ')
-    if tweet_text == None:
-        return json.dumps({
-            'tweet_text': None, 
-            'sentiment_score': None,
-            'detected_mood': None
-        }, ensure_ascii=False) 
-    lang = LANGUAGE_DETECTION.predict(tweet_text)
-    is_english = False
-    if lang[0][0] == '__label__en':
-        is_english = True
-    result = {
-        "tweet_text": tweet_text, 
-        "is_english": is_english
-    }
+    result = []
+    for res in data:
+        tweet_text = res['tweet_text']
+        tweet_text_replaced = tweet_text.replace('\n', ' ')
+        if tweet_text_replaced == None:
+            return json.dumps({
+                'tweet_text': None, 
+                'sentiment_score': None,
+                'detected_mood': None
+            }, ensure_ascii=False) 
+        lang = LANGUAGE_DETECTION.predict(tweet_text_replaced)
+        is_english = False
+        if lang[0][0] == '__label__en':
+            is_english = True
+        result.append({
+            "tweet_text": tweet_text, 
+            "is_english": is_english
+        })
     return json.dumps(result, ensure_ascii=False)
 
 @app.route('/api/sentiment-score',methods=['POST'])
 def sentiment_score():
     data = request.json
-    tweet_text = data['tweet_text']
-    tweet_text = tweet_text.replace('\n', ' ')
-    if tweet_text == None:
-        return json.dumps({
-            'tweet_text': None, 
-            'sentiment_score': None,
-            'detected_mood': None
-        }, ensure_ascii=False) 
-    prediction = SENTIMENT_SCORE.predict(tweet_text)
-    score = {}
-    for i in range(3):
-        score[prediction[0][i]] = prediction[1][i]
-    
-    result = {
-        'tweet_text': tweet_text, 
-        'sentiment_score': {
-            'positive': score['__label__POSITIVE'],
-            'neutral': score['__label__NEUTRAL'],
-            'negative': score['__label__NEGATIVE']
-        },
-        'detected_mood': prediction[0][0].split('_')[-1]
-    }
+    result = []
+    for res in data:
+        tweet_text = res['tweet_text']
+        tweet_text_replaced = tweet_text.replace('\n', ' ')
+        if tweet_text_replaced == None:
+            return json.dumps({
+                'tweet_text': None, 
+                'sentiment_score': None,
+                'detected_mood': None
+            }, ensure_ascii=False) 
+        prediction = SENTIMENT_SCORE.predict(tweet_text_replaced)
+        score = {}
+        for i in range(3):
+            score[prediction[0][i]] = prediction[1][i]
+        result.append({
+            'tweet_text': tweet_text, 
+            'sentiment_score': {
+                'positive': score['__label__POSITIVE'],
+                'neutral': score['__label__NEUTRAL'],
+                'negative': score['__label__NEGATIVE']
+            },
+            'detected_mood': prediction[0][0].split('_')[-1]
+        })
     return json.dumps(result, ensure_ascii=False)
 
 if __name__ == "__main__":
